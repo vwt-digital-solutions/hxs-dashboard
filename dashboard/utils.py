761fcf4e3d5517effeeb65803152993eceee424d
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-07-27 13:51:06 +0200

    DAT-3865. Remove cloud kms

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,10 +1,8 @@
 import io
-import base64
 import pandas as pd
 
 from google.cloud import secretmanager
 from google.cloud import storage
-from google.cloud import kms_v1
 
 client = storage.Client()
 
@@ -28,14 +26,6 @@ def download_as_dataframe(bucket_name, blob_name, sheet_name=0):
     df = pd.read_excel(buffer, sheet_name)
     buffer.close()
     return df
-
-
-def decrypt_secret(project, region, keyring, key, secret_base64):
-    secret_enc = base64.b64decode(secret_base64)
-    kms_client = kms_v1.KeyManagementServiceClient()
-    key_path = kms_client.crypto_key_path_path(project, region, keyring, key)
-    secret = kms_client.decrypt(key_path, secret_enc)
-    return secret.plaintext.decode("utf-8").replace('\n', '')
 
 
 def get_secret(project_id, secret_id, version_id='latest'):
