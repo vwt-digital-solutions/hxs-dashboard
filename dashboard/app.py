b349cb803688b793e243d8f823dccd252a6a7ec8
Bernie van Veen <b.vanveen@vwt.digital>
2020-11-02 11:40:51 +0100

    Revert "Specify redirect_url to fix redirect behind WAF. DAT-3616"
    This reverts commit 1d244b408f7c3916f3e8e5e7a90cb0270e6ed898.

--- dashboard/app.py
+++ dashboard/app.py
@@ -36,9 +36,8 @@ if config.authentication:
         auth['tenant'],
         utils.get_secret(auth['project_id'], auth['secret_name']),
         auth['required_scopes'],
-        auth.get('redirect_url', None),
         e2e_expected_audience=auth.get('e2e_expected_audience', None),
-        e2e_client_id=auth.get('e2e_client_id', None),
+        e2e_client_id=auth.get('e2e_client_id', None)
     )
 
 
